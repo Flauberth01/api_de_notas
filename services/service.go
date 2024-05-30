108f0a1e331d703f7d06db891dc9f16975c190de
flauberth01 <flauberth12@gmail.com>
2024-05-30 00:37:59 -0500

    feat(services): adiciona funções CRUD nas notas
    A exibição de notas foi refatorada e novas funções foram adicionadas: CriaNota, AtualizaNota e DeletaNota. Essas funções permitem as operações CRUD para as notas. Validações de ID foram adicionadas na AtualizaNota e DeletaNota para garantir a existência da nota antes de realizar operações.

--- services/service.go
+++ services/service.go
@@ -1,12 +1,43 @@
 package services
 
 import (
+	"errors"
+
 	"api_de_notas/database"
 	"api_de_notas/models"
 )
 
 func ExibeNotas() ([]models.Nota, error) {
 	var notas []models.Nota
-	result := database.DB.Find(&notas)
-	return notas, result.Error
+	err := database.DB.Find(&notas).Error
+	return notas, err
+}
+
+func CriaNota(nota models.Nota) error {
+	err := database.DB.Create(&nota).Error
+	return err
+}
+
+func AtualizaNota(nota models.Nota) error {
+	var notaExistente models.Nota
+
+	if err := database.DB.First(&notaExistente, nota.ID).Error; err != nil {
+		return errors.New("ID não encontrado")
+	}
+
+	err := database.DB.Model(&notaExistente).Updates(nota).Error
+
+	return err
+}
+
+func DeletaNota(id uint64) error {
+	var nota models.Nota
+
+	if err := database.DB.First(&nota, id).Error; err != nil {
+		return errors.New("ID não encontrado")
+	}
+
+	err := database.DB.Delete(&nota).Error
+
+	return err
 }
