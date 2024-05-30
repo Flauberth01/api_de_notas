f433b84c77b5be044a933dfd66b6eb355983921a
flauberth01 <flauberth12@gmail.com>
2024-05-30 00:36:28 -0500

    refactor(models): ajusta campos da estrutura Nota
    Essa alteração renomeia os campos CriadoEm e AtualizadoEm para CreatedAt e UpdatedAt, respectivamente, para melhor alinhamento com as convenções do Gorm. O tipo de dados do campo ID também foi alterado de uint para uint64 para permitir uma maior gama de identificadores únicos.

--- models/models.go
+++ models/models.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Nota struct {
-	ID           uint      `gorm:"primaryKey" json:"id"`
-	Titulo       string    `json:"titulo"`
-	Conteudo     string    `json:"conteudo"`
-	CriadoEm     time.Time `json:"criado_em"`
-	AtualizadoEm time.Time `json:"atualizado_em"`
+	ID        uint64    `gorm:"primaryKey" json:"id"`
+	Titulo    string    `json:"titulo"`
+	Conteudo  string    `json:"conteudo"`
+	CreatedAt time.Time `json:"criado_em"`
+	UpdatedAt time.Time `json:"atualizado_em"`
 }
