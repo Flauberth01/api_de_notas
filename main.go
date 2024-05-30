619b3bffcc0ecfc030eac9e2b2295aecae407259
flauberth01 <flauberth12@gmail.com>
2024-05-30 00:40:46 -0500

    feat adicionar endpoints para criar nota, atualizar e deletar

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"api_de_notas/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+	_ "api_de_notas/docs"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func main() {
@@ -15,7 +18,14 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
+	r.Get("/notas/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("/notas/swagger/doc.json"),
+	))
+
 	r.Get("/notas", handlers.ExibeNotas)
+	r.Post("/notas", handlers.CriaNota)
+	r.Put("/notas/{id}", handlers.AtualizaNota)
+	r.Delete("/notas/{id}", handlers.DeletaNota)
 
 	http.ListenAndServe(":8000", r)
 }
